1d8a0de641c719b94bcd98acdd51acca58ed1d96
maksaptz <128064182+maksaptz@users.noreply.github.com>
2023-05-31 14:17:05 +0300

    Update deb_script.sh

--- deb_script.sh
+++ deb_script.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/sh
 
 touch /etc/apt/sources.list.d/supersh.list # создаем файл для добавления репозитория
 echo 'deb [trusted=yes] http://93.88.74.208:55557 stable free' > /etc/apt/sources.list.d/supersh.list # добавляем репозиторий
